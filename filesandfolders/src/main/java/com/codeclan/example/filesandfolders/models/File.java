aa88c4f172b151dfb7313f0a92e32a01387ff58e
Anita Safar <ancsa0416@gmail.com>
2022-01-18 17:08:37 +0000

    file class extended with pojo

--- filesandfolders/src/main/java/com/codeclan/example/filesandfolders/models/File.java
+++ filesandfolders/src/main/java/com/codeclan/example/filesandfolders/models/File.java
@@ -1,15 +1,32 @@
 package com.codeclan.example.filesandfolders.models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "files")
 public class File {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "extension")
     private String extension;
+
+    @Column(name = "size")
     private String size;
 
     public File(String name, String extension, String size) {
         this.name = name;
         this.extension = extension;
         this.size = size;
+    }
+
+    public File() {
+
     }
 
     public String getName() {
@@ -34,5 +51,13 @@ public class File {
 
     public void setSize(String size) {
         this.size = size;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }

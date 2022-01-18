221a03442e8f7fb6d695a5b3626a7cba447f1a85
Anita Safar <ancsa0416@gmail.com>
2022-01-18 17:56:50 +0000

    folder class extended with pojo

--- filesandfolders/src/main/java/com/codeclan/example/filesandfolders/models/Folder.java
+++ filesandfolders/src/main/java/com/codeclan/example/filesandfolders/models/Folder.java
@@ -1,11 +1,25 @@
 package com.codeclan.example.filesandfolders.models;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "folders")
 public class Folder {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
 
     public Folder(String name) {
         this.name = name;
+    }
+
+    public Folder() {
+
     }
 
     public String getName() {
@@ -14,5 +28,13 @@ public class Folder {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }

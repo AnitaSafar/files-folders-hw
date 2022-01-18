e115e73036d8d7fac7c89f8ce961038310615541
Anita Safar <ancsa0416@gmail.com>
2022-01-18 18:20:18 +0000

    index route added to FolderController

--- filesandfolders/src/main/java/com/codeclan/example/filesandfolders/controllers/FolderController.java
+++ filesandfolders/src/main/java/com/codeclan/example/filesandfolders/controllers/FolderController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FolderController {
@@ -19,6 +21,16 @@ public class FolderController {
     @GetMapping(value = "/folders")
     public ResponseEntity<List<Folder>> getAllFolders() {
         return new ResponseEntity<>(folderRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/folders/{id}")
+    public ResponseEntity<Optional<Folder>> getFolder(@PathVariable Long id) {
+        Optional<Folder> payload = folderRepository.findById(id);
+        if(payload.isPresent()) {
+            return new ResponseEntity<>(payload, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(payload, HttpStatus.NOT_FOUND);
+        }
     }
 
 }

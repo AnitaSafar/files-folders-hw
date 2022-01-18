2318fc240bb74e99eb7630b7494329ea8beeab54
Anita Safar <ancsa0416@gmail.com>
2022-01-18 19:00:49 +0000

    create route added to UserController

--- filesandfolders/src/main/java/com/codeclan/example/filesandfolders/controllers/UserController.java
+++ filesandfolders/src/main/java/com/codeclan/example/filesandfolders/controllers/UserController.java
@@ -6,9 +6,7 @@ import com.codeclan.example.filesandfolders.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,5 +30,11 @@ public class UserController {
         } else {
             return new ResponseEntity<>(payload, HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping(value = "/users")
+    public ResponseEntity<User> postUser(@RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
     }
 }

86e2f061f689c042a9676c95336ba6e9655c4c68
Anita Safar <ancsa0416@gmail.com>
2022-01-18 17:54:16 +0000

    folder class created, initial setup done

---
+++ filesandfolders/src/main/java/com/codeclan/example/filesandfolders/models/Folder.java
@@ -0,0 +1,18 @@
+package com.codeclan.example.filesandfolders.models;
+
+public class Folder {
+
+    private String name;
+
+    public Folder(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

89139271bc4285fadb67bfba4d8f5855ab324bd5
Anita Safar <ancsa0416@gmail.com>
2022-01-18 17:48:21 +0000

    show route added

--- filesandfolders/src/main/java/com/codeclan/example/filesandfolders/controllers/FileController.java
+++ filesandfolders/src/main/java/com/codeclan/example/filesandfolders/controllers/FileController.java
@@ -1,8 +1,16 @@
 package com.codeclan.example.filesandfolders.controllers;
 
+import com.codeclan.example.filesandfolders.models.File;
 import com.codeclan.example.filesandfolders.repositories.FileRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class FileController {
@@ -10,4 +18,8 @@ public class FileController {
     @Autowired
     FileRepository fileRepository;
 
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles() {
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
 }
